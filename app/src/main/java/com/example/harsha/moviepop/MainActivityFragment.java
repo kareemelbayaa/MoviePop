d002b6ab2f16c1580b468a7e62649fd3330bfd06
HarshaLaxman <laxmanharsha.n@gmail.com>
2016-07-06 18:30:58 -0700

    .7 Connected to MovieDB .6 Made ListView a GridView .5 SettingsActivity set up with nonfunctional sort option .4 Mock Data in a Parcelable Movies object that saves on destruction of activity .3 Mock Data With Image, Name, and Date. Custom adapter and Movies class added

--- app/src/main/java/com/example/harsha/moviepop/MainActivityFragment.java
+++ app/src/main/java/com/example/harsha/moviepop/MainActivityFragment.java
@@ -1,7 +1,13 @@
 package com.example.harsha.moviepop;
 
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,8 +16,20 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by Harsha on 7/5/2016.
@@ -25,9 +43,9 @@ public class MainActivityFragment extends Fragment {
 
     //dummy data to be modified later
     Movies[] mockData = {
-            new Movies("The Gift","2015",R.drawable.cupcake),
-            new Movies("NightCrawler","2014",R.drawable.eclair),
-            new Movies("Bowfinger","2003",R.drawable.icecream)
+            new Movies("The Gift", "2015", R.drawable.cupcake),
+            new Movies("NightCrawler", "2014", R.drawable.eclair),
+            new Movies("Bowfinger", "2003", R.drawable.icecream)
     };
 
     public MainActivityFragment() {
@@ -36,10 +54,9 @@ public class MainActivityFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(savedInstanceState == null || !savedInstanceState.containsKey("moviesKey")) {
+        if (savedInstanceState == null || !savedInstanceState.containsKey("moviesKey")) {
             moviesList = new ArrayList<Movies>(Arrays.asList(mockData));
-    }
-        else {
+        } else {
             moviesList = savedInstanceState.getParcelableArrayList("moviesKey");
         }
         setHasOptionsMenu(true);
@@ -78,4 +95,124 @@ public class MainActivityFragment extends Fragment {
 
     }
 
+
+    public class UpdateMovies extends AsyncTask<String, Void, String[]> {
+
+        private final String LOG_TAG = UpdateMovies.class.getSimpleName();
+
+        //TODO: Finish this method that takes a String of JSON data and gives us what we want
+        /*
+        private String[] getMovieDataFromJson(String moviesJsonStr, String sortType)
+                throws JSONException {
+
+            return niceMovieData;
+
+        }
+        */
+
+        //Query data from MovieDB
+        @Override
+        protected String[] doInBackground(String... params) {
+
+        /*
+        // If there's no zip code, there's nothing to look up.  Verify size of params.
+        if (params.length == 0) {
+            return null;
+        }
+        */
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+
+            String format = "json";
+            String sortType = getString(R.string.pref_sort_popular);
+
+            try {
+                // Construct the URL for the MovieDB query
+                final String MOVIES_BASE_URL =
+                        "http://api.themoviedb.org/3/movie/popular?api_key=" + getString(R.string.moviedb_api_key);
+                Uri builtUri = Uri.parse(MOVIES_BASE_URL);
+                URL url = new URL(builtUri.toString());
+
+            /*
+            final String QUERY_PARAM = "q";
+            final String FORMAT_PARAM = "mode";
+            final String UNITS_PARAM = "units";
+            final String DAYS_PARAM = "cnt";
+            final String APPID_PARAM = "APPID";
+
+
+            Uri builtUri = Uri.parse(MOVIES_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, params[0])
+                    .appendQueryParameter(FORMAT_PARAM, format)
+                    .appendQueryParameter(UNITS_PARAM, units)
+                    .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                    .appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_WEATHER_MAP_API_KEY)
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+*/
+                // Create the request to MovieDB, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+
+            /* Implement method that will edit the Json Data into what we want
+            try {
+                return getWeatherDataFromJson(forecastJsonStr, numDays);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+            */
+
+            // This will only happen if there was an error getting or parsing the forecast.
+            return null;
+        }
+
+    }
 }
